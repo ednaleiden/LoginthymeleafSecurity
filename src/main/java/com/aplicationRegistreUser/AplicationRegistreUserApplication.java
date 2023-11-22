af94bd70e3e08491e5be32a5a06678b2c94d34aa
ednaleiden <ednadupont@hotmail.com>
2023-11-21 20:32:02 -0500

    start of project with Spring security

---
+++ src/main/java/com/aplicationRegistreUser/AplicationRegistreUserApplication.java
@@ -0,0 +1,13 @@
+package com.aplicationRegistreUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationRegistreUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationRegistreUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplicationRegistreUser/AplicationRegistreUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicationRegistreUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationRegistreUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
